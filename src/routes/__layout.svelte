5b563ba41a2aa91d72a95f681d0ec12cfac95296
Emadamerho-Atori Nefe <nefejames@yahoo.com>
2021-05-25 01:54:01 -0700

    transitions in sveltekit

---
+++ src/routes/__layout.svelte
@@ -0,0 +1,29 @@
+<script>
+import "../styles/reset.css"
+</script>
+
+
+<main>
+	<div class="container">
+
+		<!-- The <slot> element is where your page component will be inserted. -->
+		<slot />
+	</div>
+</main>
+
+
+
+<style>
+  * {
+	margin: 0;
+	padding: 0;
+	box-sizing: border-box;
+  }
+
+	.container{
+		padding: 3rem;
+
+	}
+
+
+</style>
